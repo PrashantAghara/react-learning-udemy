faaae17855c797003ca2213acb1dd4af15b2d631
prashant <prashant.aghara2111@gmail.com>
2022-09-18 18:05:13 +0530

    Food Order App - Day 1

---
+++ food-order-app/src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = ({}) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="meals" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
