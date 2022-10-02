6e257829e7f12a2c5884642e434b786731e653c5
prashant <prashant.aghara2111@gmail.com>
2022-10-02 16:59:22 +0530

    Custom Hook Advance Example

---
+++ custom-hook-realistic-eg/src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "./../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest } = useHttp();
+
+  const createTask = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendRequest(
+      {
+        url: "https://react-http-98968-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
