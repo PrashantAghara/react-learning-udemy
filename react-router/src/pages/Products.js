1cd533f4ecb73d422e365a3d3140ad8cdaffc997
prashant <prashant.aghara2111@gmail.com>
2022-10-10 21:57:41 +0530

    Added Dynamic Routes using React Routers

---
+++ react-router/src/pages/Products.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <>
+      <h1>Products</h1>
+      <ul>
+        <li>Book</li>
+        <li>Laptop</li>
+        <li>Phone</li>
+      </ul>
+    </>
+  );
+};
+
+export default Products;
