432e747858731eab774f37207717e6ea5e74c323
prashant <prashant.aghara2111@gmail.com>
2022-10-29 12:19:36 +0530

    Added Describe function for Test Suite

--- react-testing/src/components/Greeting.test.js
+++ react-testing/src/components/Greeting.test.js
@@ -1,12 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hello World as test", () => {
-  //Arrange
-  render(<Greeting />);
+describe("Greeting Components", () => {
+  test("renders Hello World as test", () => {
+    //Arrange
+    render(<Greeting />);
 
-  //Act
-  //Assert
-  const element = screen.getByText("Hello World!", { exact: true });
-  expect(element).toBeInTheDocument();
+    //Act
+    //Assert
+    const element = screen.getByText("Hello World!", { exact: true });
+    expect(element).toBeInTheDocument();
+  });
 });
